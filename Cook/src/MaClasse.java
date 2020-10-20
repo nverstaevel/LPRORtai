643406635a5212fb32cfc8df84109969f9dc68de
Nicolas Verstaevel <nicolas.verstaevel@ut-capitole.fr>
2020-10-20 08:28:44 +0200

    J'ai fait ma premiere classe

---
+++ Cook/src/MaClasse.java
@@ -0,0 +1,10 @@
+
+public class MaClasse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello WOrld");
+		System.out.println("Coucou");
+	}
+
+}
